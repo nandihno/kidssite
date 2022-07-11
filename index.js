56fef8b59392a5fb79b36294ca96b9b8a060331b
Nando <fernando.deleon@gmail.com>
2022-07-11 22:39:41 +1000

    adding time

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express')
+const express = require('express');
+const moment = require("moment");
+
 const app = express()
 const port = 3000
 
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    let currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    res.send('Hello there Alejandro and Fernandito it is '+currentTime);
 })
 
 app.listen(port, () => {
